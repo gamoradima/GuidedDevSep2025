4cca1cfbe61a22880a2431b13ddd772c69a06f20
gamoradima <gamoradima@gmail.com>
2025-09-26 21:16:47 +0300

    Call web service example

---
+++ UsrYacht/Schemas/UsrCompanySearchInputPage/UsrCompanySearchInputPage.js
@@ -0,0 +1,87 @@
+define("UsrCompanySearchInputPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"name": "PageTitle",
+				"values": {
+					"caption": "#MacrosTemplateString(#ResourceString(PageTitle_caption)#)#",
+					"visible": true,
+					"headingLevel": "label"
+				}
+			},
+			{
+				"operation": "insert",
+				"name": "Input_rn5n1m1",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter1_o72dmx2",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrTextParameter1_o72dmx2",
+					"multiline": false
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "insert",
+				"name": "Input_norhcn6",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"colSpan": 1,
+						"row": 2,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.PageParameters_UsrTextParameter2_ac0zyv7",
+					"labelPosition": "above",
+					"control": "$PageParameters_UsrTextParameter2_ac0zyv7",
+					"multiline": false
+				},
+				"parentName": "MainContainer",
+				"propertyName": "items",
+				"index": 1
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfigDiff: /**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [
+					"attributes"
+				],
+				"values": {
+					"PageParameters_UsrTextParameter1_o72dmx2": {
+						"modelConfig": {
+							"path": "PageParameters.UsrTextParameter1"
+						}
+					},
+					"PageParameters_UsrTextParameter2_ac0zyv7": {
+						"modelConfig": {
+							"path": "PageParameters.UsrTextParameter2"
+						}
+					}
+				}
+			}
+		]/**SCHEMA_VIEW_MODEL_CONFIG_DIFF*/,
+		modelConfigDiff: /**SCHEMA_MODEL_CONFIG_DIFF*/[
+			{
+				"operation": "merge",
+				"path": [],
+				"values": {
+					"dataSources": {}
+				}
+			}
+		]/**SCHEMA_MODEL_CONFIG_DIFF*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
